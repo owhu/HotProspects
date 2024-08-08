7242e60d15de4f4e3a95eaa536eab3b28c907928
oliver <oliverhu.oh@gmail.com>
2024-08-08 09:21:02 -0700

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Oliver Hu on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
