cb9a8df5cd310696476add46acc3d8de616ada40
oliver <oliverhu.oh@gmail.com>
2024-08-12 12:54:56 -0700

    Project updated - Added sort options

---
+++ HotProspects/SortableProspectsView.swift
@@ -0,0 +1,18 @@
+//
+//  SortableProspectsView.swift
+//  HotProspects
+//
+//  Created by Oliver Hu on 8/12/24.
+//
+
+import SwiftUI
+
+struct SortableProspectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SortableProspectsView()
+}
